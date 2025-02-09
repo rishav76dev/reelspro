6efa50e8ea13882b830547e4267a33833d4c7bce
rishav76dev <rjrishav037@gmail.com>
2025-02-09 19:49:23 +0530

    imagekit-auth added

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: NextRequest){
         if(existingUser){
             return NextResponse.json(
                 {error: "Email is already registered"},
-                { status: 400}
+                { status: 201}
             );
 
         }
